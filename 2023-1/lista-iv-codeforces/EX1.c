9ffddeb0e80eea34201ab42ec9c9bc2e84b69c7c
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2023-06-30 20:18:06 +0000

    🎉 lê a matriz e a imprime

--- 2023-1/lista-iv-codeforces/EX1.c
+++ 2023-1/lista-iv-codeforces/EX1.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void ler_matriz(int linhas, int colunas, int matriz[linhas][colunas]) {
+  for (int i = 0; i < linhas; i++) {
+    for (int j = 0; j < colunas; j++) {
+      scanf("%d", &matriz[i][j]);
+    }
+  }
+}
+
+void imprimir_matriz(int linhas, int colunas, int matriz[linhas][colunas]) {
+  for (int i = 0; i < linhas; i++) {
+    for (int j = 0; j < colunas; j++) {
+      printf("%d ", matriz[i][j]);
+    }
+    printf("\n");
+  }
+}
+
 void ler_inteiro(int *var) {
   scanf("%d", var);
 }
@@ -10,5 +27,8 @@ int main(void) {
   int colunas, linhas;
   ler_inteiro(&colunas);
   ler_inteiro(&linhas);
+  int matriz[linhas][colunas];
+  ler_matriz(linhas, colunas, matriz);
+  imprimir_matriz(linhas, colunas, matriz);
   return 0;
 }
