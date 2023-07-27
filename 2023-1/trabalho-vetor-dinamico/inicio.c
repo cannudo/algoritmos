e6d476df29cde9845e815685f093e9bc2c1e7e0b
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2023-07-27 20:03:01 +0000

    🎉 inicia o trabalho

---
+++ 2023-1/trabalho-vetor-dinamico/inicio.c
@@ -0,0 +1,6 @@
+/* [Lista duplamente ligada]
+* Estrutura de dados linear consistida de uma sequência de elementos
+* chamados "nós". Cada nó tem dois ponteiros e um valor. O primeiro
+* ponteiro aponta para um nó anterior (cabeça). O outro aponta para um nó
+* posterior (cauda).
+*/
