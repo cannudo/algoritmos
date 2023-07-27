08df3493136a0931a1ef2fdb49028e25f0eb0c78
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2023-07-27 20:26:11 +0000

    ✅ aloca memória para uma listaDuplamenteLigada

--- 2023-1/trabalho-vetor-dinamico/inicio.c
+++ 2023-1/trabalho-vetor-dinamico/inicio.c
@@ -4,3 +4,32 @@
 * ponteiro aponta para um nó anterior (cabeça). O outro aponta para um nó
 * posterior (cauda).
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct no {
+    int valor;
+    struct no* anterior;
+    struct no* proximo;
+};
+
+struct listaDuplamenteLigada {
+    struct no* cabeca;
+    struct no* cauda;
+    int size;
+};
+
+struct listaDuplamenteLigada* alocarMemoriaListaDuplamenteLigada() {
+    struct listaDuplamenteLigada* lista = (struct listaDuplamenteLigada*) malloc(sizeof(struct listaDuplamenteLigada));
+    lista->cabeca = NULL;
+    lista->cauda = NULL;
+    lista->size = 0;
+    return lista;
+};
+
+int main() {
+    struct listaDuplamenteLigada* lista = alocarMemoriaListaDuplamenteLigada();
+    printf("%d\n", lista->size);
+    return 0;
+} 
